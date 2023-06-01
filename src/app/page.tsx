a4124f036815c72df19fb49b4f963c9fbc441dad
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-05-31 21:10:44 -0400

    Updated and cleaned up main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import TextBlock from "@components/TextBlock";
-import Modal from "@components/Modal";
-import ImagePixelData from "@components/ImagePixelData";
-import sampleText from "@samples/sampleText";
+import { Modal, TextBlock, Loading } from "@/components/";
+import { Pixel, TextStyle } from "@/interfaces/";
+import { repeatText, getImageData, generateCSS } from "@/utils/";
 import localFont from "next/font/local";
-import { Pixel } from "@interfaces/";
+import sampleText from "@/samples/sampleText";
 
 const PantasiaUnlicensedTrial = localFont({
   src: "../assets/fonts/PantasiaUnlicensedTrial-Regular.woff2",
@@ -14,92 +13,46 @@ const PantasiaUnlicensedTrial = localFont({
 
 const Home: React.FC = () => {
   const [text, setText] = useState<string>(sampleText);
-  const [imagePath, setImagePath] = useState<string | null>(
-    "/samples/sampleImage.png"
-  );
+  const [imageSrc, setImageSrc] = useState<string>("/samples/sampleImage.png");
   const [imagePixels, setImagePixels] = useState<Pixel[]>([]);
+  const [imageWidth, setImageWidth] = useState<number>(0);
   const [imageHeight, setImageHeight] = useState<number>(0);
+  const [textStyle, setTextStyle] = useState<TextStyle>(TextStyle.TRANSPARENT);
+  const [loading, setLoading] = useState<boolean>(true);
 
-  function updateText(text: string) {
-    while (text.length < 19720) {
-      text += " ";
-      text += text;
+  useEffect(() => {
+    setLoading(true);
+
+    const getPixels = async () => {
+      // @TODO: update imageHeight here so that it will change in text
+      const imageData = await getImageData(imageSrc);
+      setImagePixels(imageData.pixels);
+      setImageHeight(imageData.imageHeight);
+      setImageWidth(imageData.imageWidth);
+      await generateCSS(imageData.pixels);
+      setLoading(false);
+    };
+
+    getPixels();
+  }, [imageSrc]);
+
+  useEffect(() => {
+    setLoading(true);
+
+    const updateText = async () => {
+      // @TODO: change based on imageHeight as well
+      const newText = await repeatText(text, imageWidth, imageHeight);
+      setText(newText);
+      setLoading(false);
     }
-    text = text.slice(0, 18894) + "...";
-    setText(text);
-  }
 
-  function updateImage(src: any) {
-    setImagePath(src);
-  }
-
-  function updateImagePixels(pixels: Pixel[]) {
-    setImagePixels(pixels);
-    generateCSSStylesheet(pixels);
-  }
-
-  const generateCSSStylesheet = (pixels: Pixel[]): void => {
-    const uniqueColors = new Set<string>();
-    pixels.forEach((p) => {
-      const pixelColor = `p-${p.r}-${p.g}-${p.b}`;
-      uniqueColors.add(pixelColor);
-    })
-    const colorsArray = Array.from(uniqueColors);
-
-    console.log(colorsArray);
-
-    const cssRules = colorsArray
-      .map((pixel, index) => {
-        const splitPixel = pixel.split("-");
-        const r = splitPixel[1]
-        const g = splitPixel[2]
-        const b = splitPixel[3]
-        const r_inverse = (255 - parseInt(r)).toString();
-        const g_inverse = (255 - parseInt(g)).toString();
-        const b_inverse = (255 - parseInt(b)).toString();
-        return `.p-${r}-${g}-${b}::selection { 
-                  color: rgb(${r_inverse}, ${g_inverse}, ${b_inverse});
-                  background-color: rgb(${r}, ${g}, ${b}); 
-                }`;
-      })
-      .join("\n");
-
-    // Create a style element
-    const styleElement = document.createElement("style");
-
-    // Set the CSS rules as text content of the style element
-    styleElement.textContent = cssRules;
-
-    // Append the style element to the document's head
-    document.head.appendChild(styleElement);
-
-    // // Create a blob with the CSS content
-    // const cssBlob = new Blob([cssRules], { type: "text/css" });
-
-    // // Create a URL for the blob
-    // const cssURL = URL.createObjectURL(cssBlob);
-
-    // // Create a link element to download the CSS file
-    // const linkElement = document.createElement("a");
-    // linkElement.href = cssURL;
-    // linkElement.download = "pixels.css";
-
-    // // Simulate a click event to trigger the download
-    // linkElement.click();
-
-    // // Clean up the created URL
-    // URL.revokeObjectURL(cssURL);
-  };
+    updateText();
+  }, [text, imageHeight]);
 
   return (
     <main className={PantasiaUnlicensedTrial.className}>
-      <Modal updateText={updateText} updateImage={updateImage} />
-      <TextBlock text={text} image={imagePixels} />
-      <ImagePixelData
-        imagePath={imagePath || ""}
-        targetWidth={160}
-        updateImagePixels={updateImagePixels}
-      />
+      <Modal setText={setText} setImageSrc={setImageSrc} loading={loading}/>
+      { loading ? <Loading /> : <TextBlock text={text} image={imagePixels} /> }
     </main>
   );
 };
