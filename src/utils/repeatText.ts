e02f617b57a15ca9d6d29e144f2a9e0a28b0e07f
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-05-31 21:08:53 -0400

    Repeat text utility func

---
+++ src/utils/repeatText.ts
@@ -0,0 +1,17 @@
+const repeatText = (
+  text: string,
+  imageWidth: number,
+  imageHeight: number
+): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const length: number = imageWidth * imageHeight;
+    while (text.length < length) {
+      text += " ";
+      text += text;
+    }
+    text = text.slice(0, length - 3) + "... ";
+    resolve(text);
+  });
+};
+
+export default repeatText;
