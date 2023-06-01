5caab748be8c052808feaf3729ffb86bfaff46da
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-05-31 21:10:29 -0400

    Updated getImageData, testing sizes

---
+++ src/utils/getImageData.ts
@@ -0,0 +1,50 @@
+import { ImageData, Pixel } from "@/interfaces/";
+
+// @TODO: getImageData
+// @TODO: determine image size logic here
+const getImageData = async (imageUrl: string): Promise<ImageData> => {
+  try { 
+    const response = await fetch(imageUrl);
+    const arrayBuffer = await response.arrayBuffer();
+    const imageBitmap = await createImageBitmap(new Blob([arrayBuffer]));
+  
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+  
+    console.log(imageBitmap.width, imageBitmap.height);
+
+    const aspectRatio = imageBitmap.width / imageBitmap.height;
+    const targetWidth = 180;
+    const targetHeight = Math.floor(targetWidth / aspectRatio);
+
+    console.log(targetWidth, targetHeight);
+    
+    canvas.width = targetWidth;
+    canvas.height = targetHeight;
+
+    context?.drawImage(imageBitmap, 0, 0, targetWidth, targetHeight);
+  
+    const imageData = context?.getImageData(0, 0, targetWidth, targetHeight);
+    const pixels: Pixel[] = [];
+  
+    if (imageData) {
+      const data = imageData.data;
+  
+      for (let i = 0; i < data.length; i += 4) {
+        const pixel: Pixel = {
+          r: data[i],
+          g: data[i + 1],
+          b: data[i + 2],
+        };
+        pixels.push(pixel);
+      }
+    }
+  
+    return { pixels: pixels, imageWidth: targetWidth, imageHeight: targetHeight };
+  } catch (err) {
+    console.log(err);
+    return { pixels: [], imageWidth: 0, imageHeight: 0 };
+  }
+};
+
+export default getImageData;
