1ce9e70d58b4540942dbb92785314cf06ecf7bf2
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-06-03 03:25:43 -0400

    Added documentation, cleaned up code

--- src/components/TextBlock.tsx
+++ src/components/TextBlock.tsx
@@ -1,14 +1,23 @@
+/* 
+ * This file generates the body of text. The overall div contains individual 
+ * divs representing rows of pixels, split according to the image scale, and 
+ * each row contains multiple span elements, each representing one pixel, with
+ * each having a class name based on the RGB values of the corresponding pixel 
+ * in the source image.
+ * 
+ */
+
 import React from "react";
 import styles from "@/styles/textblock.module.css";
-import { TextBlockProps, Pixel } from "@/interfaces/";
+import { TextBlockProps } from "@/interfaces/";
 
-const TextBlock: React.FC<TextBlockProps> = (props: TextBlockProps) => {
-  let text: string[] = props.text.split("");
-  let image: Pixel[] = props.image;
-
+const TextBlock: React.FC<TextBlockProps> = ({ text, image, imgScale }) => {
   const rows = [];
-  for (let i = 0; i < text.length; i += 180) {
-    const rowCharacters = text.slice(i, i + 180);
+  const length = imgScale;
+  const splitText = text.replace(/(\r\n|\n|\r)/gm, " ").split("");
+
+  for (let i = 0; i < splitText.length; i += length) {
+    const rowCharacters = splitText.slice(i, i + length);
 
     const row = (
       <div className={styles.row} key={i}>
@@ -27,7 +36,7 @@ const TextBlock: React.FC<TextBlockProps> = (props: TextBlockProps) => {
     rows.push(row);
   }
 
-  return <div className={styles.text}>{rows}</div>;
+  return <div id="textBlock" className={styles.text}>{rows}</div>;
 }
 
 export default TextBlock;
