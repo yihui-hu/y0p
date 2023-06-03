6e530a8a116f8dc97a356c194ff654cad79a5a64
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-06-03 03:26:25 -0400

    Added documentation

--- src/utils/getImageData.ts
+++ src/utils/getImageData.ts
@@ -1,33 +1,39 @@
+/**
+ * This file takes in an image URL and scale, retrieves the image data, resizes
+ * it, and returns the pixel data along with the new width and height based
+ * on the image scale.
+ *
+ */
+
 import { ImageData, Pixel } from "@/interfaces/";
 
-// @TODO: determine image size logic here
-const getImageData = async (imageUrl: string): Promise<ImageData> => {
-  try { 
+const getImageData = async (
+  imageUrl: string,
+  imgScale: number
+): Promise<ImageData> => {
+  try {
     const response = await fetch(imageUrl);
     const arrayBuffer = await response.arrayBuffer();
     const imageBitmap = await createImageBitmap(new Blob([arrayBuffer]));
-  
-    const canvas = document.createElement('canvas');
-    const context = canvas.getContext('2d');
 
-    console.log(window.innerHeight);
-    console.log(window.innerWidth);
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
 
     const aspectRatio = imageBitmap.width / imageBitmap.height;
-    const targetWidth = 180;
+    const targetWidth = imgScale;
     const targetHeight = Math.floor(targetWidth / aspectRatio);
-    
+
     canvas.width = targetWidth;
     canvas.height = targetHeight;
 
     context?.drawImage(imageBitmap, 0, 0, targetWidth, targetHeight);
-  
+
     const imageData = context?.getImageData(0, 0, targetWidth, targetHeight);
     const pixels: Pixel[] = [];
-  
+
     if (imageData) {
       const data = imageData.data;
-  
+
       for (let i = 0; i < data.length; i += 4) {
         const pixel: Pixel = {
           r: data[i],
@@ -37,11 +43,11 @@ const getImageData = async (imageUrl: string): Promise<ImageData> => {
         pixels.push(pixel);
       }
     }
-  
-    return { pixels: pixels, imageWidth: targetWidth, imageHeight: targetHeight };
+
+    return { pixels: pixels, width: targetWidth, height: targetHeight };
   } catch (err) {
     console.log(err);
-    return { pixels: [], imageWidth: 0, imageHeight: 0 };
+    return { pixels: [], width: 0, height: 0 };
   }
 };
 
