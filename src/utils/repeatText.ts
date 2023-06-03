0c5e62c7b88923aed356744a648596812fafbbca
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-06-03 03:26:42 -0400

    Added documentation

--- src/utils/repeatText.ts
+++ src/utils/repeatText.ts
@@ -1,10 +1,14 @@
-const repeatText = (
-  text: string,
-  imageWidth: number,
-  imageHeight: number
-): Promise<string> => {
-  return new Promise((resolve, reject) => {
-    const length: number = imageWidth * imageHeight;
+/*
+ * The function takes in a string (i.e. text from the input in the modal) and
+ * the size of an image, and repeats the string to fit the specified image size.
+ * 
+ */
+
+import { ImageSize } from "@/interfaces";
+
+const repeatText = (text: string, imageSize: ImageSize): Promise<string> => {
+  return new Promise((resolve) => {
+    const length: number = imageSize.width * imageSize.height;
     while (text.length < length) {
       text += " ";
       text += text;
