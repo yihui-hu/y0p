86836a3102ed4328ced5b2e9429388920728ca6e
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-06-03 11:43:06 -0400

    Updated favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body>{children}</body>
     </html>
   )
