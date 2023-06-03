ea1f22a2171b93213b0243da927970e3be3cdf7e
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-06-03 03:27:03 -0400

    Added documentation, simplified useEffect calls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+/*
+ * This file contains the structure and logic for the main page of the site.
+ * Type is set in Pantasia by Wei Huang.
+ *
+ */
+
 "use client";
 
 import { useState, useEffect } from "react";
 import { Modal, TextBlock, Loading } from "@/components/";
-import { Pixel, TextStyle } from "@/interfaces/";
+import { Pixel, TextStyle, ImageSize } from "@/interfaces/";
 import { repeatText, getImageData, generateCSS } from "@/utils/";
 import localFont from "next/font/local";
 import sampleText from "@/samples/sampleText";
@@ -12,60 +18,70 @@ const PantasiaUnlicensedTrial = localFont({
 });
 
 const Home: React.FC = () => {
-  const [text, setText] = useState<string>(sampleText);
-  const [imageSrc, setImageSrc] = useState<string>("/samples/sampleImage.png");
-  const [imagePixels, setImagePixels] = useState<Pixel[]>([]);
-  const [imageWidth, setImageWidth] = useState<number>(0);
-  const [imageHeight, setImageHeight] = useState<number>(0);
+  const [inputText, setInputText] = useState<string>(sampleText);
+  const [text, setText] = useState<string>("");
+  const [imgSrc, setImgSrc] = useState<string>("/samples/sampleImage.png");
+  const [imgPixels, setImgPixels] = useState<Pixel[]>([]);
+  const [imgSize, setImgSize] = useState<ImageSize>({ width: 0, height: 0 });
+  const [imgScale, setImgScale] = useState<number>(140);
   const [textStyle, setTextStyle] = useState<TextStyle>(TextStyle.TRANSPARENT);
   const [loading, setLoading] = useState<boolean>(true);
-  const [filesURL, setFilesURL] = useState<string>("");
+
+  const updatePixels = async () => {
+    setLoading(true);
+    const imageData = await getImageData(imgSrc, imgScale);
+    setImgPixels(imageData.pixels);
+    setImgSize({ width: imageData.width, height: imageData.height });
+    await generateCSS(imageData.pixels, textStyle);
+  };
+
+  const updateText = async () => {
+    setLoading(true);
+    const newText = await repeatText(inputText, imgSize);
+    setText(newText);
+  };
+
+  const updateTextStyle = async () => {
+    setLoading(true);
+    await generateCSS(imgPixels, textStyle);
+  };
 
   useEffect(() => {
-    setLoading(true);
-
-    const getPixels = async () => {
-      const imageData = await getImageData(imageSrc);
-      setImagePixels(imageData.pixels);
-      setImageHeight(imageData.imageHeight);
-      setImageWidth(imageData.imageWidth);
-      await generateCSS(imageData.pixels, textStyle, setFilesURL);
-      setLoading(false);
-    };
-
-    getPixels();
-  }, [imageSrc]);
+    updatePixels();
+    setTimeout(() => setLoading(false), 300);
+  }, [imgSrc]);
 
   useEffect(() => {
-    setLoading(true);
-
-    const updateTextStyle = async () => {
-      await generateCSS(imagePixels, textStyle, setFilesURL);
-      setLoading(false);
-    };
-
     updateTextStyle();
+    setLoading(false);
   }, [textStyle]);
 
   useEffect(() => {
-    const updateText = async () => {
-      const newText = await repeatText(text, imageWidth, imageHeight);
-      setText(newText);
-    };
-
     updateText();
-  }, [text, imageWidth, imageHeight]);
+    setLoading(false);
+  }, [inputText, imgSize]);
+
+  useEffect(() => {
+    updatePixels();
+    updateText();
+    setLoading(false);
+  }, [imgScale])
 
   return (
     <main className={PantasiaUnlicensedTrial.className}>
       <Modal
-        setText={setText}
-        setImageSrc={setImageSrc}
+        setText={setInputText}
+        setImgSrc={setImgSrc}
         setTextStyle={setTextStyle}
-        filesURL={filesURL}
+        setImgScale={setImgScale}
+        imgScale={imgScale}
         loading={loading}
       />
-      {loading ? <Loading /> : <TextBlock text={text} image={imagePixels} />}
+      {loading ? (
+        <Loading />
+      ) : (
+        <TextBlock text={text} image={imgPixels} imgScale={imgScale} />
+      )}
     </main>
   );
 };
